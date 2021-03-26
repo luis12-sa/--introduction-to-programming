d3b2a57bc9f57251e56d6d874260dfa5d7d37794
luis miguel <migelromero333@gmail.com>
2021-03-26 14:24:27 -0500

    Create Main.java

---
+++ -introduction-to-programming/ejercicios java/P1_T32 caballo de mar/src/usta/sistemas/Main.java
@@ -0,0 +1,39 @@
+package usta.fis;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*AUTHOR: luis miguel romero
+         DATE: 2020/03/10
+         DESCRIPTION: this software shows a relief
+         */
+	System.out.println("________$$$$..");
+	System.out.println("______$$$$$$$$$");
+	System.out.println("______$$$$$$$_$");
+	System.out.println("_____$$$$$$$$$$");
+	System.out.println("______$$$$$$$$$$");
+	System.out.println("_____$$$$$$_$$$$$");
+	System.out.println("____$$$$$$$_____$$$");
+	System.out.println("____$$$$$$$$_____$");
+	System.out.println("____$$$$$$$$$$");
+	System.out.println("_____$$$$$$$$$$");
+	System.out.println("_____$$$$$$$$$$$");
+	System.out.println("______$$$$$$$$$$$");
+	System.out.println("_$$$$___$$$$$$$$$");
+	System.out.println("__$$$$$$$$$$$$$$$");
+	System.out.println("_$$$$$$$$$$$$$$$");
+	System.out.println("__$$$$$$$$$$$$$");
+	System.out.println("$$$$$$$$$$$$$");
+	System.out.println("__$__$$$$$$");
+	System.out.println("____$$$$$$");
+	System.out.println("____$$$$$");
+	System.out.println("___$$$$$$_____$");
+	System.out.println("___$$$$$$___$$_$$");
+	System.out.println("____$$$$$___$__$$");
+	System.out.println("____$$$$$______$$");
+	System.out.println("_____$$$$$____$$$");
+	System.out.println("_______$$$$$$$$$");
+	System.out.println("__________$$$$");
+
+    }
+}
